c5975eb8648dedc314d01e9f1f6ab185337e50c1
Ian Torres <iantorres@outlook.com>
2025-01-06 18:03:22 -0300

    Forms fields added.

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,7 +1,9 @@
 import PanelItem from './components/PanelItem.vue'
+import FormPanel from './fields/Form/Panel.vue'
 import Panel from './fields/Detail/Panel.vue'
 
 Nova.booting((app, store) => {
+  app.component('FormPanel', FormPanel)
   app.component('PanelItem', PanelItem)
   app.component('DetailPanel', Panel)
   // app.component('preview-gridder', PreviewField)
